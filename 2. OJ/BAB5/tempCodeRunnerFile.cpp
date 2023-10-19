a8df6f89d2906e05a55d397f15ba9ee1e842f364
CaptainOrb <135736141+CaptainOrb@users.noreply.github.com>
2023-10-19 08:38:46 +0700

    Delete Unfinished Folders

--- 2. OJ/BAB5/tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
-    // memset(matrix1, 0, size * sizeof(matrix1[1][1]));
-    // memset(matrix2, 0, size * sizeof(matrix2[1][1]));
-    // memset(matrix3, 0, size * sizeof(matrix3[1][1]));
-    // memset(matrix12, 0, size * sizeof(matrix12[1][1]));
-    // memset(matrix123, 0, size * sizeof(matrix123[1][1]));
